572a1e153110d19b3fd70ae77be3f782bb6834ca
Vimalraj17 <30496666+Vimalraj17@users.noreply.github.com>
2017-08-04 16:52:26 +0530

    Update stringtoint.c

--- stringtoint.c
+++ stringtoint.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-int n;
+int n,i;
 char s[n];
 scanf("%d",&n);
 for(i=0;i<1;i++)
  scanf("%c",&s[i]);
-for(i=o;i<n;i++)
+for(i=0;i<n;i++)
  printf("%d",s[n]);
  return 0;
  }
